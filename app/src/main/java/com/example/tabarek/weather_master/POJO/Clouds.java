91fac0a442e459fdafc1578811cfc727bbb7fc8a
Tabarek <tabarekaljaaf@yahoo.com>
2019-02-07 12:54:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tabarek/weather_master/POJO/Clouds.java
@@ -0,0 +1,26 @@
+package com.example.tabarek.weather_master.POJO;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+/**
+ * Created by SuhailMirji on 11/03/2018.
+ */
+
+public class Clouds implements Serializable {
+
+    @SerializedName("all")
+    @Expose
+    private Integer all;
+
+    public Integer getAll() {
+        return all;
+    }
+
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+}
